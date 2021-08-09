7794638599e2d72cb836e3500f2fc5fe682a4ff6
Natalie Barba <nbarba2010@gmail.com>
2021-08-09 15:16:49 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Natalie Barba
+// Using const since we never have to change the values below
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor((Math.random() * 10) + 1);
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(-13, 12, 5, -34, 20);
